852692548fdaad78cc49b0b6c49b4c345859fd7f
Clayt Hulin <Clayt@Macintosh.local>
2015-05-19 13:41:53 -0500

    Initial Commit

---
+++ ESPN 1420/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ESPN 1420
+//
+//  Created by Clayt Hulin on 5/19/15.
+//  Copyright (c) 2015 ESPN 1420. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ESPN 1420/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ESPN 1420
+//
+//  Created by Clayt Hulin on 5/19/15.
+//  Copyright (c) 2015 ESPN 1420. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
